74afd77d5b5fd628352649b625d2652867a921fd
JK <john.kokotinis@tective.nl>
2026-08-14 08:00:45 +0200

    Add inspection definitions

---
+++ app/inspection.py
@@ -0,0 +1,12 @@
+PRE_FLIGHT_ITEMS = [
+    "Airframe condition", "Propellers", "Motors", "Landing gear", "Battery",
+    "Battery contacts", "Payload / camera", "GNSS / GPS", "Sensors", "LEDs",
+    "Remote controller", "Cables / connectors", "Communications", "Firmware",
+    "Physical damage"
+]
+
+POST_FLIGHT_ITEMS = [
+    "Airframe damage", "Propellers after flight", "Motors / abnormal noise",
+    "Battery condition", "Battery temperature", "Payload / camera condition",
+    "Landing gear", "Sensors", "Cables / connectors", "General cleanliness"
+]
